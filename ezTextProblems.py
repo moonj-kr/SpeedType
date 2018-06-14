d11f2efd8a1364e21543e0d8cd13b0b08c1c7d30
Jisook Moon <moonj.kr@gmail.com>
2018-06-15 01:12:59 +0900

    input refresh eztext in progress

---
+++ ezTextProblems.py
@@ -0,0 +1,78 @@
+import pygame, time, random, eztext
+from ScrapTestFiles.Type import text_objects
+
+pygame.init()
+
+# variables
+lst = []
+display_width = 1000
+display_height = 800
+black = (0,0,0)
+white = (255, 255, 255)
+green = (0,100,0)
+
+def text_objects(text, font):
+    textSurface = font.render(text,True, white)
+    return textSurface, textSurface.get_rect()
+
+# fn that shows user input
+def show_input(events):
+    textbox.set_pos(105, 740)
+    textbox.update(events)
+    pygame.draw.rect(window, white, ((100, 735), (250, 30))) # input box!!
+    textbox.draw(window)
+    pygame.display.flip()
+
+def show_input_more(events, textbox):
+    textbox.set_pos(105, 740)
+    textbox.update(events)
+    pygame.draw.rect(window, white, ((100, 735), (250, 30))) # input box!!
+    textbox.draw(window)
+    pygame.display.flip()
+
+################ Window Setup ###################
+
+# window setup
+font = pygame.font.SysFont("Veranda", 30)
+window = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption("Speed Type")
+clock = pygame.time.Clock()
+pygame.draw.rect(window, white, ((100, 735), (250, 30)))
+pygame.display.update()
+textbox = eztext.Input(maxlength=70, color=(black), prompt='')
+
+################ Game Loop ###################
+def game_loop():
+    crashed = False
+
+    while crashed == False:
+        events = pygame.event.get()
+        for event in events:
+            # window exiting
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        # word display
+            if event.type == pygame.KEYDOWN:
+                if(event.key==pygame.K_RETURN):
+                    print("success")
+
+                    pygame.draw.rect(window, white, ((100, 735), (250, 30)))
+                    pygame.display.update()
+                    textbox1 = eztext.Input(maxlength=70, color=(black), prompt='')
+                    show_input_more(events,textbox1)
+                    pygame.display.update()
+                    time.sleep(5)
+                    continue
+
+        # update window
+        show_input(events)
+        pygame.display.update()
+
+        # fps
+        clock.tick(200)
+
+if __name__ == '__main__':
+    game_loop()
+    pygame.quit()
+    quit()
